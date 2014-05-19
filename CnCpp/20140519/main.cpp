4a0af2c08306b04d322b326aca7a97f98e8814b2
jklim <gocobe1228@naver.com>
2014-05-19 15:50:41 +0900

    파일 입출력
    ..

--- CnCpp/20140519/main.cpp
+++ CnCpp/20140519/main.cpp
@@ -1,12 +1,35 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <conio.h>
 
 using namespace std;
 
 int main()
 {
+	const char* filename = "item.save";
 
-	// 주석 추가.
+	fstream file;
+
+	// write
+	//file.open(filename, ios_base::out | ios_base::binary);
+
+	//int i;
+
+	//cout << "Enter integer : ";
+	//cin >> i;
+	//file.write((const char*)&i, sizeof(int));
+
+	// read
+	file.open(filename, ios_base::in | ios_base::binary);
+
+	int i;
+	file.read((char*)&i, sizeof(int));
+
+	cout << i;
+
+
+	file.close();
+
 	_getch();
 	return 0;
 }
