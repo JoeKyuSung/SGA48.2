17c80186a0b3105450525635bb9d24a25c28f00a
jklim <gocobe1228@naver.com>
2014-05-20 15:59:30 +0900

    윈도우 기본틀 프로젝트 추가함.
    윈도우 기본틀 프로젝트 추가함.

---
+++ Win32API/20140520/winmain.cpp
@@ -0,0 +1,77 @@
+﻿#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+INT WINAPI _tWinMain(HINSTANCE hInstance,
+					 HINSTANCE hPrevInstance,
+					 LPTSTR lpCmdLine,
+					 INT nCmdShow)
+{
+	LPCTSTR szClassName = _T("MyMainWindowClass");
+
+	WNDCLASSEX wcex;
+
+	::memset(&wcex, 0, sizeof(wcex));
+
+	wcex.cbSize = sizeof(wcex);
+	wcex.style = CS_VREDRAW | CS_HREDRAW;
+	wcex.lpfnWndProc = &WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szClassName;
+	wcex.hIconSm = ::LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!::RegisterClassEx(&wcex))
+	{
+		return 1;
+	}
+
+	int cx = ::GetSystemMetrics(SM_CXSCREEN);
+
+	HWND hWnd = ::CreateWindowEx(0, szClassName, _T("Win32 Test"),
+		WS_OVERLAPPEDWINDOW,
+		0, 0,
+		500, 500,
+		NULL, NULL, hInstance, NULL);
+
+	if (hWnd == NULL)
+	{
+		return 2;
+	}
+
+	::ShowWindow(hWnd, SW_NORMAL);
+	::UpdateWindow(hWnd);
+
+	MSG msg;
+
+	while (true)
+	{
+		//if (::GetMessage(&msg, NULL, 0, 0))
+		if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+		if (msg.message == WM_QUIT)
+			break;
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_DESTROY)
+	{
+		::PostQuitMessage(0);
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
