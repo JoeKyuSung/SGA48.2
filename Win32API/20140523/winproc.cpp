172b25483e3949bfea287d8b120849440e2c6591
jklim <gocobe1228@naver.com>
2014-05-23 15:01:31 +0900

    쓸데없는 코드 삭제.

--- Win32API/20140523/winproc.cpp
+++ Win32API/20140523/winproc.cpp
@@ -2,27 +2,8 @@
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	const int count = 100;
-	static POINT pt[count];
-	const LONG radius = 30;
-
 	if (uMsg == WM_CREATE)
 	{
-		RECT rc;
-		::GetClientRect(hWnd, &rc);
-		::InflateRect(&rc, -radius, -radius);
-
-		LONG cx = rc.right - rc.left;
-		LONG cy = rc.bottom - rc.top;
-
-		for (int i = 0; i < count; i++)
-		{
-			pt[i].x = rand()%cx + radius;
-			pt[i].y = rand()%cy + radius;
-		}
-
-		pt[0].x = (rc.left + rc.right)/2;
-
 		return 0;
 	}
 	else if (uMsg == WM_DESTROY)
@@ -36,89 +17,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HDC hdc = ::BeginPaint(hWnd, &ps);
 
 		// TODO.....
-		RECT rc;
-		::GetClientRect(hWnd, &rc);
-		LONG a = (rc.left + rc.right)/2;
-
-		for (int i = 0; i < count; i++)
-		{
-			if (a > pt[i].x)
-			{
-				HBRUSH hBrush = ::CreateSolidBrush(RGB(255,0,0));
-				HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
-
-				HPEN hPen = ::CreatePen(PS_SOLID, 1, RGB(255,120,120));
-				HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
-
-				::Ellipse(hdc, pt[i].x - radius,
-								pt[i].y - radius,
-								pt[i].x + radius,
-								pt[i].y + radius);
-
-				::SelectObject(hdc, hOldPen);
-				::DeleteObject(hPen);
-
-				::SelectObject(hdc, hOldBrush);
-				::DeleteObject(hBrush);
-			}
-			else if (a == pt[i].x)
-			{
-				HBRUSH hBrush = ::CreateSolidBrush(RGB(0,0,255));
-				HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
-
-				HPEN hPen = ::CreatePen(PS_SOLID, 1, RGB(120,120,255));
-				HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
-
-				::Rectangle(hdc, pt[i].x - radius,
-								pt[i].y - radius,
-								pt[i].x + radius,
-								pt[i].y + radius);
-
-				::SelectObject(hdc, hOldPen);
-				::DeleteObject(hPen);
-
-				::SelectObject(hdc, hOldBrush);
-				::DeleteObject(hBrush);
-
-				hBrush = ::CreateSolidBrush(RGB(255,0,0));
-				hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
-
-				hPen = ::CreatePen(PS_SOLID, 1, RGB(255,120,120));
-				hOldPen = (HPEN)::SelectObject(hdc, hPen);
-
-				::Ellipse(hdc, pt[i].x - radius,
-								pt[i].y - radius,
-								pt[i].x + radius,
-								pt[i].y + radius);
-
-				::SelectObject(hdc, hOldPen);
-				::DeleteObject(hPen);
-
-				::SelectObject(hdc, hOldBrush);
-				::DeleteObject(hBrush);
-
-			}
-			else
-			{
-				HBRUSH hBrush = ::CreateSolidBrush(RGB(0,0,255));
-				HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
-
-				HPEN hPen = ::CreatePen(PS_SOLID, 1, RGB(120,120,255));
-				HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
-
-				::Rectangle(hdc, pt[i].x - radius,
-								pt[i].y - radius,
-								pt[i].x + radius,
-								pt[i].y + radius);
-
-				::SelectObject(hdc, hOldPen);
-				::DeleteObject(hPen);
-
-				::SelectObject(hdc, hOldBrush);
-				::DeleteObject(hBrush);
-			}
-
-		}
 
 		::EndPaint(hWnd, &ps);
 		return 0;
